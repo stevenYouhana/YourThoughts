f7ecac9784971c5fad034016a3a749a4569665bf
Steven Youhana <stevenyouhana@hotmail.co.nz>
2020-01-13 12:53:41 +1300

    edded routes to production

--- server.js
+++ server.js
@@ -6,27 +6,27 @@ const cors = require('cors');
 const port = process.env.PORT || 5000;
 const apiRoutes = require('./backend/routes/api');
 
-app.use(cors());
+// app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 if (process.env.NODE_ENV === 'production') {
-
+console.log("if (process.env.NODE_ENV === 'production'): ",process.env.NODE_ENV)
   app.use(express.static(path.join(__dirname, 'client/build')));
-
+  apiRoutes(app);
   app.get('*', (req, res) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-      
+    // res.header("Access-Control-Allow-Origin", "*");
+    // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
     res.sendFile(path.join(__dirname+'/client/public/index.html'));
   });
-  apiRoutes(app);
+
 }
-app.get('/', (req, res) => {
-  console.log("app.get('/', (req, res)");
-  res.send("app.get('/'")
-});
+// app.get('/', (req, res) => {
+//   console.log("app.get('/', (req, res)");
+//   res.send("app.get('/'")
+// });
 
 app.get('/hey', (req, res) => res.send('ho!'));
 
