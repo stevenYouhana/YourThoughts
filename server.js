6d01f4f2ddf8176a6b9469d0a4a5db6a551c7c30
Steven Youhana <stevenyouhana@hotmail.co.nz>
2020-01-13 12:45:10 +1300

    edded routes to production

--- server.js
+++ server.js
@@ -2,19 +2,26 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 const apiRoutes = require('./backend/routes/api');
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 if (process.env.NODE_ENV === 'production') {
+
   app.use(express.static(path.join(__dirname, 'client/build')));
 
   app.get('*', (req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      
     res.sendFile(path.join(__dirname+'/client/public/index.html'));
   });
+  apiRoutes(app);
 }
 app.get('/', (req, res) => {
   console.log("app.get('/', (req, res)");
